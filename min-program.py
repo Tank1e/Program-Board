73b4a8efb8fbcd560a2fd07db64e22ef09ec6a82
Tankie <15038603990@163.com>
2019-10-18 22:00:51 +0800

    成功把form信息写入数据库

--- min-program.py
+++ min-program.py
@@ -4,41 +4,73 @@ from flask import render_template
 from flask import redirect
 from flask import url_for
 import mysql.connector
+
 app = Flask(__name__)
-@app.route('/home',methods=['GET','POST'])
+
+
+#这是主界面，对应的静态文件为home.html
+@app.route('/',methods=['GET','POST'])
 def home():
     if request.method == 'GET':
         return render_template('home.html')
     else:
         return redirect(url_for('commit'))
+
+#这是提交页面，由‘/’主界面post跳转
 @app.route('/commit',methods=['GET','POST'])
 def commit():
     if request.method == 'GET':
         return render_template('commit.html')
     else:
-        return redirect(url_for('greet'))
+        #return redirect(url_for('greet'))
+        write_date()
+        return '您的提交成功了'
 
+
+#这是一个测试页面
 @app.route('/hello')
 def greet():
     return 'hello world!'
 
+
+#这是一个测试页面
 @app.route('/login',methods=['GET','POST'])
 def login():
     if request.method == 'POST':
         return do_the_login()
     else:
         return show_the_login_form()
-@app.route('/',methods=['GET'])
-def write_date():
+#@app.route('/',methods=['GET','POST'])
+
+#def write_date():
+#    mydb = mysql.connector.connect(host="localhost",user="root",passwd="123123",database="test_for_board")
+#    mycu = mydb.cursor()
+#    sql = "insert into text (name,context) values (%s,%s)"
+#    val = ('tankie','Hello,your guy from web!')
+#    mycu.execute(sql,val)
+#    mydb.commit()
+#    mycu.close()
+#    mydb.close()
+#    return 'hello'
+
+#if request.method == 'POST':
+#这是向数据库写入信息的函数数据库信息
+#：host="localhost",user="root",passwd="123123",database="test_for_board"
+def write_date():              
+    print (request.form.get('name'),request.form.get('text'))            
     mydb = mysql.connector.connect(host="localhost",user="root",passwd="123123",database="test_for_board")
-    mycu = mydb.cursor()
+    mycu = mydb.cursor()       
+    temp_text = {'name':'Hello','text':'hello from test'}
+    #sql = "insert into text (name,context) values (%s,%s)" % (temp_text['name'],temp_text['text'])
     sql = "insert into text (name,context) values (%s,%s)"
-    val = ('tankie','Hello,your guy from web!')
+    val = (request.form.get('name'),request.form.get('text'))
+    #val = ('localtest','localtest')
     mycu.execute(sql,val)
     mydb.commit()
+    print('done')
     mycu.close()
     mydb.close()
-    return 'hello'
+
 
 if __name__=='__main__':
     app.run()
--- test.py
+++ test.py
@@ -3,8 +3,10 @@ def write_date():
     print ('hello')
     mydb = mysql.connector.connect(host="localhost",user="root",passwd="123123",database="test_for_board")
     mycu = mydb.cursor()
+    temp_text = {'name':'Hello','text':'hello from test'}
+    #sql = "insert into text (name,context) values (%s,%s)" % (temp_text['name'],temp_text['text'])
     sql = "insert into text (name,context) values (%s,%s)"
-    val = ('tankie','Hello,your guy!')
+    val = (request.args.get('name'),request.args.get('text'))
     mycu.execute(sql,val)
     mydb.commit()
     print('done')
